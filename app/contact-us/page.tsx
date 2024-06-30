58b8cdcbd7982ee2bc2fb7ad4025a6304e4f4d45
rishabhkr-r111 <rishabhkrr111@gmail.com>
2024-06-30 23:00:40 +0530

    .............

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -1,7 +1,7 @@
 
 export default function ContactUs() {
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-900 text-white">
+    <div className="flex justify-center items-center h-screen text-white">
       <div className="bg-gray-800 p-8 rounded shadow-md max-w-md w-full">
         <h1 className="text-3xl font-bold mb-4 text-center">Contact Us</h1>
         <form className="space-y-4">
@@ -36,7 +36,6 @@ export default function ContactUs() {
             <textarea
               id="message"
               name="message"
-              rows="4"
               className="border border-gray-700 rounded px-3 py-2 mt-1 focus:outline-none focus:ring-2 focus:ring-blue-500 bg-gray-900 text-white"
               placeholder="Your Message"
             ></textarea>
