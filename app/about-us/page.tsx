396d76344e590f63df362cfa7084b9f9fdae74bd
rishabhkr-r111 <rishabhkrr111@gmail.com>
2024-06-30 23:16:28 +0530

    ...........

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,8 +1,6 @@
 export default function About() {
   return (
-    // Changed background to gradient
-    <div className="min-h-screen bg-gradient-to-r from-gray-700 via-gray-900 to-black flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-      {/* Added bg-opacity and hover effect */}
+    <div className="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-3xl w-full space-y-8 bg-gray-800 bg-opacity-90 p-8 rounded-lg shadow-lg transform transition duration-500 hover:scale-105">
         <header>
           <h1 className="text-center text-4xl font-extrabold text-white">About Us</h1>
@@ -12,7 +10,6 @@ export default function About() {
         </header>
         <section className="space-y-6">
           <div>
-            {/* Added SVG icon */}
             <h2 className="text-2xl font-bold text-white flex items-center">
               <svg className="w-6 h-6 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 16h-1v-4h-1m4 4h-1v-4h-1m-2 8H5a2 2 0 01-2-2V6a2 2 0 012-2h6l2 2h6a2 2 0 012 2v8a2 2 0 01-2 2h-4"></path></svg>
               Our Mission
